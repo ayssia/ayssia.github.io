c3ecc18476a72de6727d85c603ae06660ad09da0
ayssia <nynaevealmearah@gmail.com>
2021-03-24 16:03:56 +0800

    Add files via upload

---
+++ src.ex
@@ -0,0 +1,23 @@
+{
+    "info":
+    {
+        "id": "com.ayssia.test",
+        "name": "Ayssia源",
+        "author": "Ayssia",
+        "email": "",
+        "description": "测试",
+        "icon": "https://raw.githubusercontent.com/moxcomic/moxcomic.github.io/master/icon.png"
+    },
+    "release":
+    [
+        {
+            "id": "kxmj",
+            "name": "科学麻将",
+            "description": "快乐",
+            "author": "Ayssia",
+            "version": "1.0.0",
+            "preview": "",
+            "homepage": "https://github.com/ayssia/test.git"
+        }
+    ]
+}
